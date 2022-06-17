0fc001a846dcb34e7d50518b61f860689d15cd0a
gyawali9 <roshan.gyawali@codehimalaya.com>
2022-06-17 20:13:08 +0545

    sidemenu component creation

---
+++ src/App/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import SideMenu from "../components/SideMenu";
+import "./App.css";
+const App = () => {
+  return (
+    <>
+      <SideMenu />
+      <div>Here we go</div>
+    </>
+  );
+};
+
+export default App;
